03af3af9cbfe3f27afd7a1418f9879e54ba5b445
xxRohitxx <rohitrs319791@gmail.com>
2022-12-18 00:04:46 +0530

    rohit

--- frontend/expedia/src/pages/product/Fetchproducts.jsx
+++ frontend/expedia/src/pages/product/Fetchproducts.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import FetchData  from './fetchData'
 import axios from "axios"
-
+import LoadingSkeleton from './skeleton'
+import { Box, Select } from '@chakra-ui/react'
 
 
 const Fetchproducts = () => {
 const [data,setData] = useState([]);
- const [loading,setLoading] = useState(true);
+
 
 const getdata=()=>{
-  axios.get("https://adorable-pear-earrings.cyclic.app/hotel").then((res)=>setData(res.data)).then(setLoading(false))
+  axios.get("https://adorable-pear-earrings.cyclic.app/hotel").then((res)=>setData(res.data))
   }
 
 
@@ -17,7 +18,9 @@ const getdata=()=>{
 getdata()
   },[])
 
-
+const sortingFunc=(e)=>{
+ console.log(e.target.value)
+}
 
  
 
@@ -26,11 +29,17 @@ getdata()
   return (
     <div>
       {
-        loading && <h2>Loading...</h2>
+       !data[0] && <LoadingSkeleton/>
       }
+       <Box margin="10px" flex="1">
+            <Select onChange={sortingFunc} placeholder='Recomended' size='lg'>
+                <option value='High'>Price:High to low</option>
+                <option value='low'>Price:low to High</option>
+                </Select>
+            </Box>
 
       {data[0] && data.map((ele)=>(
-      <FetchData key={ele._id} ele={ele}/>
+      <FetchData  key={ele._id} ele={ele}/>
       ))}
       
     </div>
---
+++ frontend/expedia/src/pages/product/map.jsx
@@ -0,0 +1,7 @@
+const Map=()=>{
+return(
+   <iframe width="100%" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=patna&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+)
+}
+
+export default Map
