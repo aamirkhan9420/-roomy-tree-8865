a793c3b524d21006b57755dc8719a0ff5d03248c
Niraj Kumar <83273099+nirajraj98@users.noreply.github.com>
2022-12-17 21:06:53 +0530

    Admin

--- frontend/expedia/src/App.js
+++ frontend/expedia/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Router from './routes/router';
+// import Router from './routes/router';
 import AllRoute from "./components/Admin/AdminRoute";
 
 function App() {
   return (
     <div className="App">
+
       <AllRoute />
       
       {/* <Router /> */}
